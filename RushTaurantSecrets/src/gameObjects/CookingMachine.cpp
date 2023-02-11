0b92e7e1cb93ed1f79bc69d49efe607426b12136
nanxiqin <nanxiqin@ucm.es>
2023-02-11 15:10:03 +0100

    Plantilla CookingMachine

--- RushTaurantSecrets/src/gameObjects/CookingMachine.cpp
+++ RushTaurantSecrets/src/gameObjects/CookingMachine.cpp
@@ -1,2 +1,24 @@
 
 #include "CookingMachine.h"
+
+pair<_ecs::_dish_id, bool> CookingMachine::canFormDish() {
+	return dishComb->existDish(ing);
+}
+
+void CookingMachine::cook(_ecs::_dish_id d) {
+	dish = d;
+	state = cooking;
+}
+
+
+void CookingMachine::finishCooking() {
+	state = finished;
+}
+
+_ecs::_dish_id CookingMachine::pickDish() {
+	_ecs::_dish_id dish_aux = dish;
+	state = available;
+	dish = _ecs::NONE_DISH;
+	return dish_aux;
+}
+
--- RushTaurantSecrets/src/gameObjects/CookingMachine.h
+++ RushTaurantSecrets/src/gameObjects/CookingMachine.h
@@ -1,20 +1,37 @@
 #pragma once
 
-#include <unordered_map>
-#include "../structure/Food_def.h"
+#include "../structure/DishCombinator.h"
 using namespace std;
 class CookingMachine
 {
 public:
 	//determinar si el horno est¨¢ libre, cocinando o que ha cocinado un plato
-	enum State{ available,cooking,finished };
-	//informar si se puede formar un plato y qu¨¦ plato es
-	pair<_ecs::_dish_id, bool> canFormDish(vector<_ecs::_ingredients_id*>ing);
-	//cocinar un plato
-	void cook(_ecs::_dish_id* d);
+	enum State { available, cooking, finished };
 private:
 	State state;
-	_ecs::_dish_id* dish; // plato que lleva cocinando,null si no hay
+	DishCombinator* dishComb; //solo existe uno en todo el juego
+	_ecs::_dish_id dish; // plato que lleva cocinando,none si no hay
+	vector<_ecs::_ingredients_id>* ing; //puntero a nube de ingredientes que tiene el jugador
+
+	//ha terminado de cocinar el plato
+	void finishCooking();
+
+public:
+	CookingMachine(DishCombinator* _dishComb, vector<_ecs::_ingredients_id>* _ing) :
+		state(available), dish(_ecs::_dish_id::NONE_DISH), ing(_ing), dishComb(_dishComb) {};
+
+	//consultar el plato que tiene
+	_ecs::_dish_id getDish() { return dish; }
+
+	//informar si se puede formar un plato y qu¨¦ plato es
+	pair<_ecs::_dish_id, bool> canFormDish();
+	//pair<_ecs::_dish_id, bool> canFormDish(vector<_ecs::_ingredients_id>ing); (VERSION ANTIGUA)
+
+	//cocinar un plato
+	void cook(_ecs::_dish_id d);
+
+	//recoger el plato
+	_ecs::_dish_id pickDish();
 
 };
 
