bc7a232d3811fca1b0de32a3b31abd12efe635c8
nanxiqin <nanxiqin@ucm.es>
2023-02-12 17:31:12 +0100

    diseño supermercado en tilemap

---
+++ RushTaurantSecrets/assets/tilemaps/7_Art_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="7_Art_48x48" tilewidth="48" tileheight="48" tilecount="112" columns="16">
+ <image source="../LimeZu_modern_interiors/1_Interiors/Theme_Sorter_48x48/7_Art_48x48.png" width="768" height="336"/>
+</tileset>
---
+++ RushTaurantSecrets/assets/tilemaps/9_Fishing_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="9_Fishing_48x48" tilewidth="48" tileheight="48" tilecount="432" columns="16">
+ <image source="../LimeZu_modern_interiors/1_Interiors/Theme_Sorter_48x48/9_Fishing_48x48.png" width="768" height="1296"/>
+</tileset>
