ef344eda09039902fb220a77e07f694e01e870e5
nanxiqin <nanxiqin@ucm.es>
2023-02-12 17:42:54 +0100

    Arreglo tilemap de supermercado

--- RushTaurantSecrets/assets/tilemaps/comida.tsx
+++ RushTaurantSecrets/assets/tilemaps/comida.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="comida" tilewidth="48" tileheight="96" tilecount="42" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="comida" tilewidth="48" tileheight="96" tilecount="38" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="6">
   <image width="31" height="30" source="../../../../../../../Desktop/Assets/cabbage.png"/>
@@ -14,117 +14,105 @@
   <image width="30" height="25" source="../../../../../../../Desktop/Assets/potato.png"/>
  </tile>
  <tile id="16">
-  <image width="48" height="96" source="../../../../../../../Desktop/Assets/Group 1.png"/>
+  <image width="48" height="96" source="../others/Group 1.png"/>
  </tile>
  <tile id="14">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cabbage_p.png"/>
- </tile>
- <tile id="15">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cabbage_p.png"/>
- </tile>
- <tile id="17">
-  <image source="../../../../../../../Desktop/Assets/cartel/cabbage_p.png"/>
- </tile>
- <tile id="18">
-  <image source="../../../../../../../Desktop/Assets/cartel/cabbage_p.png"/>
- </tile>
- <tile id="19">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cabbage_p.png"/>
+  <image width="48" height="48" source="../others/cabbage (2).png"/>
  </tile>
  <tile id="20">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/7.png"/>
+  <image width="48" height="50" source="../others/7.png"/>
  </tile>
  <tile id="22">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/9.png"/>
+  <image width="48" height="50" source="../others/9.png"/>
  </tile>
  <tile id="26">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/13.png"/>
+  <image width="48" height="48" source="../others/13.png"/>
  </tile>
  <tile id="27">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/3.png"/>
+  <image width="48" height="50" source="../others/3.png"/>
  </tile>
  <tile id="28">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/5.png"/>
+  <image width="48" height="50" source="../others/5.png"/>
  </tile>
  <tile id="29">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/6.png"/>
+  <image width="48" height="48" source="../others/6.png"/>
  </tile>
  <tile id="34">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/0.png"/>
+  <image width="48" height="48" source="../others/0.png"/>
  </tile>
  <tile id="35">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/10.png"/>
+  <image width="48" height="50" source="../others/10.png"/>
  </tile>
  <tile id="36">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/11.png"/>
+  <image width="48" height="50" source="../others/11.png"/>
  </tile>
  <tile id="37">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/12.png"/>
+  <image width="48" height="50" source="../others/12.png"/>
  </tile>
  <tile id="38">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/13.png"/>
+  <image width="48" height="48" source="../others/13.png"/>
  </tile>
  <tile id="39">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/14.png"/>
+  <image width="48" height="50" source="../others/14.png"/>
  </tile>
  <tile id="40">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/15.png"/>
+  <image width="48" height="50" source="../others/15.png"/>
  </tile>
  <tile id="41">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/16.png"/>
+  <image width="48" height="50" source="../others/16.png"/>
  </tile>
  <tile id="42">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/17.png"/>
+  <image width="48" height="50" source="../others/17.png"/>
  </tile>
  <tile id="43">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/18.png"/>
+  <image width="48" height="50" source="../others/18.png"/>
  </tile>
  <tile id="44">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/19.png"/>
+  <image width="48" height="50" source="../others/19.png"/>
  </tile>
  <tile id="45">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/20.png"/>
+  <image width="48" height="50" source="../others/20.png"/>
  </tile>
  <tile id="46">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/21.png"/>
+  <image width="48" height="50" source="../others/21.png"/>
  </tile>
  <tile id="47">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/22.png"/>
+  <image width="48" height="50" source="../others/22.png"/>
  </tile>
  <tile id="48">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/23.png"/>
+  <image width="48" height="50" source="../others/23.png"/>
  </tile>
  <tile id="49">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/8.png"/>
+  <image width="48" height="50" source="../others/8.png"/>
  </tile>
  <tile id="50">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/25.png"/>
+  <image width="48" height="50" source="../others/25.png"/>
  </tile>
  <tile id="51">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/24.png"/>
+  <image width="48" height="50" source="../others/24.png"/>
  </tile>
  <tile id="52">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/2.png"/>
+  <image width="48" height="50" source="../others/2.png"/>
  </tile>
  <tile id="53">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/1.png"/>
+  <image width="48" height="50" source="../others/1.png"/>
  </tile>
  <tile id="54">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/4.png"/>
+  <image width="48" height="50" source="../others/4.png"/>
  </tile>
  <tile id="55">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/7.png"/>
+  <image width="48" height="50" source="../others/7.png"/>
  </tile>
  <tile id="56">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/5.png"/>
+  <image width="48" height="50" source="../others/5.png"/>
  </tile>
  <tile id="57">
-  <image width="48" height="48" source="../../../../../../../Desktop/Assets/cartel/6.png"/>
+  <image width="48" height="48" source="../others/6.png"/>
  </tile>
  <tile id="58">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/3.png"/>
+  <image width="48" height="50" source="../others/3.png"/>
  </tile>
  <tile id="59">
-  <image width="48" height="50" source="../../../../../../../Desktop/Assets/cartel/cabbage_p.png"/>
+  <image source="../../../../../../../Desktop/Assets/cartel/cabbage_p.png"/>
  </tile>
 </tileset>
