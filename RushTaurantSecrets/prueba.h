8b865e5f01ace7528ead32b1c0ec9a03aab6d14b
Jjianuo <Jwen@ucm.es>
2023-02-14 13:34:53 +0100

    pruebas anim

--- RushTaurantSecrets/prueba.h
+++ RushTaurantSecrets/prueba.h
@@ -3,13 +3,16 @@
 #include "PruebaComponent.h"
 #include "src/structure/Structure_def.h"
 #include "src/sdlutils/SDLUtils.h"
+#include "src/components/Animator.h"
 class Scene;
 class prueba:public GameObject
 {
 public:
 	prueba(Scene* scene) :GameObject(scene) {
 		auto& sdl = *SDLUtils::instance();
-		new PruebaComponent( &(sdl.images().at("MERMELADA")), this,_ecs::cmp_TRANSFORM);
+
+		new Animator(this, &(sdl.images().at("prueba")), 6);
+		//new PruebaComponent( &(sdl.images().at("prueba")), this,_ecs::cmp_TRANSFORM);
 	};
 	
 };
--- RushTaurantSecrets/src/components/Animator.h
+++ RushTaurantSecrets/src/components/Animator.h
@@ -6,21 +6,7 @@
 #include "../sdlutils/SDLUtils.h"
 
 using namespace std;
-
-struct Anim {
-	Texture* animSS;
-	int sqrx, sqry, width, height, maxFrames;
-
-	//n es el numero de frames que tiene la animacion
-	Anim(Texture* t, int w, int h, int n) {
-		animSS = t;
-		sqrx = 0;
-		sqry = 0;
-		width = w;
-		height = h;
-		maxFrames = n;
-	}
-};
+using Anim = std::pair<Texture*, int>;
 
 class Animator : public Component
 {
@@ -30,20 +16,41 @@ class Animator : public Component
 	int count;
 	float lastFrame;
 	float frameRate;
+	SDL_Rect sqr;
 
 public:
-	constexpr static _ecs::id_type id = _ecs::cmp_ANIMATOR;
+	//constexpr static _ecs::id_type id = _ecs::cmp_ANIMATOR;
+	constexpr static _ecs::id_type id = _ecs::cmp_TRANSFORM;
 
-	Animator(GameObject* parent) : Component(parent, id){
+	Animator(GameObject* parent, Texture* t, const int n) : Component(parent, id){
+		addAnimation(t, n);
 		currentAnim = 0;
 		count = 0;
 		lastFrame = sdlutils().currRealTime();
 		frameRate = 1000 / 60;
-
+		sqr.x = 0;
+		sqr.y = 0;
+		sqr.w = 32;
+		sqr.x = 32;
+		cout << "anim created" << endl;
 	};
 	~Animator() {};
 
-	void addAnimation(Texture* t, int w, int h, int n);
+	void addAnimation(Texture* t, int n);
 	void updateAnim();
+
+	void update() {
+		updateAnim();
+	}
+	virtual void render()
+	{
+		SDL_Rect temp;
+		temp.x = 50;
+		temp.y = 100;
+		temp.w = 32;
+		temp.h = 32;
+		//if (!animations.empty())
+		animations[currentAnim].first->render( sqr);
+	};
 };
 
