d3eeeab3a9c08c2db51a9ebb4f04f56645e54191
frmolla <frmolla@ucm.es>
2023-02-17 02:16:23 +0100

    Movimiento con teclado + intento de seguro para otros mandos

--- RushTaurantSecrets/src/components/PlayerMovementController.h
+++ RushTaurantSecrets/src/components/PlayerMovementController.h
@@ -10,8 +10,10 @@
 class PlayerMovementController : public Component
 {
 private:
-	int offset = 8;
+	int offset = 6;
+	bool key = false;
 	Vector speed = (0,0);
+	Vector aux = speed;
 
 	Transform* transform = nullptr;
 	InputHandler* input;
@@ -25,19 +27,30 @@ public:
 		input->clean();
 	}
 	virtual void handleEvents() {
-		input->refresh();
 		if (input->joysticksInitialised())
 		{
+			input->refresh();
 			// eje x mando 1
 			if (input->xvalue(0, 1) > 0 || input->xvalue(0, 1) < 0)
-			{
 				speed.setX(offset * input->xvalue(0,1));
-			}
 			// eje y mando 1
 			else if (input->yvalue(0, 1) > 0 || input->yvalue(0, 1) < 0)
-			{
 				speed.setY(offset * input->yvalue(0,1));
+			else if (input->numButtons() >= 11) {
+				// derecha
+				if (input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_RIGHT))
+					speed.setX(offset);
+				// izquierda
+				else if (input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_LEFT))
+					speed.setX(-offset);
+				// arriba		
+				else if (input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_UP))
+					speed.setY(-offset);
+				// abajo
+				else if (input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_DOWN))
+					speed.setY(offset);
 			}
+			
 			//// eje x mando 2
 			//else if (input->xvalue(0, 2) > 0 || input->xvalue(0, 2) < 0)
 			//{
@@ -49,26 +62,30 @@ public:
 			//	speed.setY(offset * input->yvalue(0,2));
 			//}
 		}
-		if (input->joysticksInitialised())
-		{
-			// derecha
-			if (input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_RIGHT)) 
-				speed.setX(offset);
-			// izquierda
-			else if (input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_LEFT)) 
-				speed.setX(-offset);
-			// arriba		
-			else if(input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_UP))
+		else if (input->keyDownEvent()){
+			key = true;
+			// arriba
+			if (input->isKeyDown(SDLK_w))
 				speed.setY(-offset);
+			// izquierda
+			else if (input->isKeyDown(SDLK_a))
+				speed.setX(-offset);
 			// abajo
-			else if (input->getButtonState(0, SDL_CONTROLLER_BUTTON_DPAD_DOWN)) 
+			else if (input->isKeyDown(SDLK_s))
 				speed.setY(offset);
+			// derecha
+			else if (input->isKeyDown(SDLK_d))
+				speed.setX(offset);
 		}
 	}
-	virtual void update() {
-		
+	virtual void update() {	
 		transform->setVel(speed);
-		speed = Vector(0,0);
+		if (input->keyUpEvent())
+			key = false;
+		if (!key || (speed.getX() - aux.getX() != 0 && speed.getY() - aux.getY() != 0)) {
+			speed = Vector(0, 0);
+			aux = speed;
+		}
 	}
 };
 
