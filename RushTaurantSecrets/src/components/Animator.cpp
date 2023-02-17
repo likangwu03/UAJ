d4fb9338456138a18d27e4295fd3b116e8fbcff0
nanxiqin <nanxiqin@ucm.es>
2023-02-17 12:51:33 +0100

    Modificación Animator
    -Traslado de implementación a cpp
    -Nuevos métodos y constructor

--- RushTaurantSecrets/src/components/Animator.cpp
+++ RushTaurantSecrets/src/components/Animator.cpp
@@ -1,1 +1,80 @@
 #include "Animator.h"
+
+Animator::Animator(GameObject* parent, Texture* t, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE) : Component(parent, id), sdl(SDLUtils::instance()) {
+	texture = t;
+	setCurrentAnim(iniFrame, endFrame, currAnim);
+	lastTic = sdlutils().currRealTime();
+	frameRate = 100;
+	parentOrientation = parent->getOrientation();
+	currOrientation = parent->getOrientation();
+};
+
+Animator::Animator(GameObject* parent, string s, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE) : Component(parent, id), sdl(SDLUtils::instance()) {
+	texture = &((*sdl).images().at(s));
+	//fila de la animacion
+	setCurrentAnim(iniFrame, endFrame, currAnim);
+	lastTic = sdlutils().currRealTime();
+	frameRate = frRate;
+};
+/*mover al siguiente frame*/
+void Animator::updateAnim() {
+	++currFrame;
+
+	if (currFrame >= endFrame)
+		currFrame = initFrame;
+}
+/*cambiar de frame*/
+void Animator::setCurrentAnim(int iniFram, int endFram, int currAnim = 0)
+{
+	currentAnim = currAnim;
+	endFrame = endFram;
+	initFrame = iniFram;
+	currFrame = iniFram;
+}
+/*cambiar de textura dada la textura*/
+void Animator::setTexture(Texture* t, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE) {
+	texture = t;
+	setCurrentAnim(iniFrame, endFrame, currAnim);
+}
+/*cambiar de textura dado el nombre de textura*/
+void Animator::setTexture(string s, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE) {
+	texture = &((*sdl).images().at(s));
+	setCurrentAnim(iniFrame, endFrame, currAnim);
+}
+void Animator::update() {
+	if (parent->getComponent<PlayerMovementController>() != nullptr) {
+		if (!parent->getComponent<PlayerMovementController>()->isMoving()) currentAnim = 1;
+		else currentAnim = 2;
+	}
+	parentOrientation = parent->getOrientation();
+	if (currOrientation != parentOrientation) {
+		currOrientation = parentOrientation;
+		if (currOrientation == east) {
+			setCurrentAnim(0, 6, currentAnim);
+		}
+		else if (parent->getOrientation() == north) {
+			setCurrentAnim(6, 12, currentAnim);
+		}
+		else if (parent->getOrientation() == west) {
+			setCurrentAnim(12, 18, currentAnim);
+		}
+		else if (parent->getOrientation() == south) {
+			setCurrentAnim(18, 24, currentAnim);
+		}
+	}
+	if (sdlutils().currRealTime() - lastTic > frameRate) {
+		lastTic = sdlutils().currRealTime();
+		updateAnim();
+	}
+
+}
+void Animator::render()
+{
+	SDL_Rect temp;
+	temp.x = parent->getComponent<Transform>()->getPos().getX();
+	temp.y = parent->getComponent<Transform>()->getPos().getY();
+	temp.w = 48;
+	temp.h = 96;
+	// indicas la columna y la fila del frame del spritesheet que quieres que se renderice
+	texture->renderFrame(temp, currFrame, currentAnim);
+};
--- RushTaurantSecrets/src/components/Animator.h
+++ RushTaurantSecrets/src/components/Animator.h
@@ -2,15 +2,16 @@
 #include <vector>
 #include "../structure/Component.h"
 #include "../structure/Structure_def.h"
-#include "../sdlutils/Texture.h"
 #include "../sdlutils/SDLUtils.h"
-#include "../structure/GameObject.h"
 #include "../components/Transform.h"
+#include "../structure/GameObject.h"
+#include "../sdlutils/Texture.h"
 #include "../components/PlayerMovementController.h"
-
 using namespace std;
 class Animator : public Component
 {
+	static const int FRAMERATE = 100;
+private:
 	Texture* texture;
 	//fila de la anim
 	int currentAnim;
@@ -19,78 +20,27 @@ class Animator : public Component
 	//columna de la anim
 	int currFrame;
 	float lastTic;
-	float frameRate;
+	int frameRate;
+	SDLUtils* sdl;
 	GOOrientation parentOrientation;
 	GOOrientation currOrientation;
-
 public:
 	constexpr static _ecs::_cmp_id id = _ecs::cmp_ANIMATOR;
 
-	Animator(GameObject* parent, Texture* t, int iniFrame, int endFrame, int currAnim = 0) : Component(parent, id) {
-		texture = t;
-		setCurrentAnim(iniFrame, endFrame, currAnim);
-		lastTic = sdlutils().currRealTime();
-		frameRate = 100;
-		parentOrientation = parent->getOrientation();
-		currOrientation = parent->getOrientation();
-	};
-	virtual ~Animator() {};
+	Animator(GameObject* parent, Texture* t, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE);
+	Animator(GameObject* parent, string s, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE);
+	
+	/*mover al siguiente frame*/
+	void updateAnim();
+	/*cambiar de frame*/
+	void setCurrentAnim(int iniFram, int endFram, int currAnim = 0);
+	/*cambiar de textura dada la textura*/
+	void setTexture(Texture* t, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE);
+	/*cambiar de textura dado el nombre de textura*/
+	void setTexture(string s, int iniFrame, int endFrame, int currAnim = 0, int frRate = FRAMERATE);
+	/*cambiar de frameRate*/
+	void setframeRate(int frRate) { frameRate = frRate; };
 
-	void updateAnim() {
-		++currFrame;
-
-		if (currFrame >= endFrame)
-			currFrame = initFrame;
-	}
-	void setCurrentAnim(int iniFram, int endFram, int currAnim = 0)
-	{
-		currentAnim = currAnim;
-		endFrame = endFram;
-		initFrame = iniFram;
-		currFrame = iniFram;
-	}
-
-	void update() {
-		if (parent->getComponent<PlayerMovementController>() != nullptr) {
-			if (!parent->getComponent<PlayerMovementController>()->isMoving()) {
-				currentAnim = 1;
-			}
-			else
-				currentAnim = 2;
-		}
-
-		parentOrientation = parent->getOrientation();
-		if (currOrientation != parentOrientation) {
-			currOrientation = parentOrientation;
-			if (currOrientation == east) {
-				setCurrentAnim(0, 6, currentAnim);
-			}
-			else if (parent->getOrientation() == north) {
-				setCurrentAnim(6, 12, currentAnim);
-			}
-			else if (parent->getOrientation() == west) {
-				setCurrentAnim(12, 18, currentAnim);
-			}
-			else if (parent->getOrientation() == south) {
-				setCurrentAnim(18, 24, currentAnim);
-			}
-		}
-
-		if (sdlutils().currRealTime() - lastTic > frameRate) {
-			lastTic = sdlutils().currRealTime();
-			updateAnim();
-		}
-
-	}
-	virtual void render()
-	{
-		SDL_Rect temp;
-		temp.x = parent->getComponent<Transform>()->getPos().getX();
-		temp.y = parent->getComponent<Transform>()->getPos().getY();
-		temp.w = 48;
-		temp.h = 96;
-
-		// indicas la columna y la fila del frame del spritesheet que quieres que se renderice
-		texture->renderFrame(temp, currFrame, currentAnim);
-	};
+	virtual void update();
+	virtual void render();
 };
