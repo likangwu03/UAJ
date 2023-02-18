4c398dbb465cbc8a5dcf27318952d8d33f4ba5b3
nanxiqin <nanxiqin@ucm.es>
2023-02-18 22:50:19 +0100

    comentado

--- RushTaurantSecrets/src/components/CashRegisterTrigger.cpp
+++ RushTaurantSecrets/src/components/CashRegisterTrigger.cpp
@@ -1,11 +1,11 @@
 #include "CashRegisterTrigger.h"
 
 void CashRegisterTrigger::isOverlapping() {
-	if (other_->getComponent<PlayerMovementController>() == nullptr)return; //si no es el jugador, no hace nada
-	if (!ih->isKeyDown(SDLK_SPACE)) return; //si no ha interactuado, no hace nada
+	//if (other_->getComponent<PlayerMovementController>() == nullptr)return; //si no es el jugador, no hace nada
+	//if (!ih->isKeyDown(SDLK_SPACE)) return; //si no ha interactuado, no hace nada
 	
 	//si se detecta que hay clientes en la caja
 	// coge el dinero del cliente y lo pasa al contador de diner
-	money->addMoney(_ecs::Dishes[clientManager->client->getOrderedDish()].price);
+	//money->addMoney(_ecs::Dishes[clientManager->client->getOrderedDish()].price);
 	//informa al cliente de que ya puede irse
 }
