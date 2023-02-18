986b41335bee212a779c184ce4eacff0ed0b7fa4
likang03 <likangwu@ucm.es>
2023-02-18 13:01:57 +0100

    Ajusta colisión de player

--- RushTaurantSecrets/src/gameObjects/Player.h
+++ RushTaurantSecrets/src/gameObjects/Player.h
@@ -13,7 +13,7 @@ public:
 	Player(Scene * scene) :GameObject(scene, _ecs::grp_PLAYER, _ecs::hdr_PLAYER) {
 		new Transform(this, Vector(100, 100), Vector(0, 0), 48, 96, 0);
 		new PlayerMovementController(this);
-		new CollisionComp(this, Vector(0, 0), (float)48, (float)48);
+		new CollisionComp(this, { 0,48 }, 48 * sdlutils().getResizeFactor(), 48 * sdlutils().getResizeFactor());
 		new Animator(this, "Player_1", 6, 2);
 		new Ingredients(this);
 }
