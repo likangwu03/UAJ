ad1d83793ba60820bfd80d799597205423ecc189
maria <marsache@ucm.es>
2023-02-19 01:57:18 +0100

    corrección MapCreator

--- RushTaurantSecrets/src/components/MapCreator.cpp
+++ RushTaurantSecrets/src/components/MapCreator.cpp
@@ -172,11 +172,7 @@ void MapCreator::createObject() {
 						aabb.width * resizeFactor, aabb.height * resizeFactor);
 				}
 				if (name == "Bin") {
-					new Bin(scene, { aabb.left * (float)resizeFactor - (float)offsetX * tileW,aabb.top * (float)resizeFactor - (float)offsetY * tileH },
-						aabb.width * resizeFactor, aabb.height * resizeFactor);
-				}
-				if (name == "Bin") {
-					new Bin(scene, { aabb.left * (float)resizeFactor - (float)offsetX * tileW,aabb.top * (float)resizeFactor - (float)offsetY * tileH },
+					new Bin(scene, { aabb.left * resizeFactor, aabb.top * resizeFactor },
 						aabb.width * resizeFactor, aabb.height * resizeFactor);
 				}
 			};
