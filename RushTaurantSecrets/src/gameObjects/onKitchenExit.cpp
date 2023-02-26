5fef34be787c1ed7c453c293c6794e6e097ba3c7
RubiaLuque <prubia01@ucm.es>
2023-02-26 21:29:35 +0100

    trigger cocina (sin implementar)

--- RushTaurantSecrets/src/gameObjects/onKitchenExit.cpp
+++
@@ -1,5 +0,0 @@
-#include "onKitchenExit.h"
-
-void onKitchenExit::onTriggerExit() {
-	ing->removeAllIngredients(); //hay que modificar este método para que devuelva los ingredientes a su sitio y no solo los elimine del player
-}
