249d89064bbf73be7c4931a19d0b056c6e0e9648
RubiaLuque <prubia01@ucm.es>
2023-03-01 11:47:56 +0100

    archivos en conflicto restaurados

--- RushTaurantSecrets/src/components/KitchenTriggerComp.h
+++ RushTaurantSecrets/src/components/KitchenTriggerComp.h
@@ -8,7 +8,6 @@ private:
 public:
 
 	KitchenTriggerComp(GameObject* parent, Vector pos_, float width_, float height_) : TriggerComp(parent, pos_, width_, height_) {
-		//ing = (static_cast<Ingredients*>(parent->getScene()->);
 	};
 	virtual void onTriggerExit();
 };
---
+++ RushTaurantSecrets/src/gameObjects/KitchenSurfaceObj.h
@@ -0,0 +1,13 @@
+#include "../structure/GameObject.h"
+#include "../components/Transform.h"
+#include "../components/KitchenTriggerComp.h"
+
+class KitchenSurfaceObj : public GameObject {
+private:
+
+public:
+	KitchenSurfaceObj(Scene* scene, Vector pos, float w, float h) : GameObject(scene, _ecs::grp_COLLISIONS) {
+		new Transform(this, pos, Vector(0, 0), w, h);
+		new KitchenTriggerComp(this, pos, w, h);
+	}
+};
