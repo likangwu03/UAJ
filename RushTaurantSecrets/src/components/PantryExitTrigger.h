e1cbde0389c09fc7ed83c10141b32022ae41a400
maria <marsache@ucm.es>
2023-03-06 17:23:03 +0100

    corrección pantry exit trigger

--- RushTaurantSecrets/src/components/PantryExitTrigger.h
+++ RushTaurantSecrets/src/components/PantryExitTrigger.h
@@ -7,6 +7,6 @@ private:
 	SceneManager* sceneM;
 	InputHandler* ih;
 public:
-	PantryExitTrigger(GameObject* parent, Vector pos_, float width_, float height_) : TriggerComp(parent, pos_, width_, height_){};
+	PantryExitTrigger(GameObject* parent, Vector pos_, float width_, float height_);
 	virtual void isOverlapping();
 };
