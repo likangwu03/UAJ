adeefff60ac612fa9b9152b33d91e7dc3a353777
nanxiqin <nanxiqin@ucm.es>
2023-03-06 17:49:10 +0100

    Arreglo Ingredients (versión compilable)

--- RushTaurantSecrets/src/gameObjects/KitchenSurfaceObj.h
+++ RushTaurantSecrets/src/gameObjects/KitchenSurfaceObj.h
@@ -8,6 +8,6 @@ private:
 public:
 	KitchenSurfaceObj(Scene* scene, Vector pos, float w, float h) : GameObject(scene, _ecs::grp_INTERACTABLE) {
 		new Transform(this, pos, Vector(0, 0), w, h);
-		new KitchenTriggerComp(this, pos, w, h);
+		new KitchenTriggerComp(this, Vector(0, 0), w, h);
 	}
 };
