844e27df19d24c72848dbc0640849776ff4b3531
RubiaLuque <prubia01@ucm.es>
2023-03-06 17:47:22 +0100

    Cambio en pos del player al cambiar de escena (debería funcionar)

--- RushTaurantSecrets/src/components/goToPantryTrigger.cpp
+++ RushTaurantSecrets/src/components/goToPantryTrigger.cpp
@@ -7,4 +7,5 @@ goToPantryTrigger::goToPantryTrigger(GameObject* parent, Vector pos_, float widt
 void goToPantryTrigger::onTriggerEnter() {
 	sceneM->setResize(false);
 	sceneM->changeScene(static_cast<Restaurant*>(parent->getScene())->getPantry(), -1);
+	parent->getScene()->getGameObject(_ecs::hdr_PLAYER)->getComponent<Transform>()->setPos(Vector(950,700));
 }
