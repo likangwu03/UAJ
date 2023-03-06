cbc786dc6a845f2fc326f1a0c923f62f3ade8f9c
RubiaLuque <prubia01@ucm.es>
2023-03-06 15:29:00 +0100

    Cambios tile pantry: señalización salida + objeto trigger salida

--- RushTaurantSecrets/src/gameObjects/goToPantryObj.h
+++ RushTaurantSecrets/src/gameObjects/goToPantryObj.h
@@ -7,6 +7,6 @@ class goToPantryObj : public GameObject {
 public:
 	goToPantryObj(Scene* scene, Vector pos, float w, float h) : GameObject(scene, _ecs::grp_INTERACTABLE) {
 		new Transform(this, pos, Vector(0, 0), w, h);
-		new goToPantryTrigger(this, Vector(0,0), w, h);
+		new goToPantryTrigger(this, Vector(0, 0), w, h);
 	}
 };
