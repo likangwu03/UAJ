c84e537a667d117f861af68d0a1e75aed51fb00c
maria <marsache@ucm.es>
2023-03-08 17:04:28 +0100

    nueva forma de guardar los ingredientes en la cesta

--- RushTaurantSecrets/src/components/BasketMarketComponent.h
+++ RushTaurantSecrets/src/components/BasketMarketComponent.h
@@ -6,6 +6,7 @@
 #include "Transform.h"
 #include "../structure/CollisionsManager.h"
 #include "../structure/CartelManager.h"
+#include "../components/Image.h"
 
 using namespace std;
 
@@ -16,12 +17,23 @@ private:
 	const int ING_SIZE = 50;
 	const int MAX_ING = 12;
 
+	struct ingText {
+		Texture* text;
+		int amount;
+	};
+
 	// lista de ingredientes en la cesta y su cantidad
-	unordered_map<_ecs::_ingredients_id, int> ingredients;
+	unordered_map<_ecs::_ingredients_id, ingText> ingredients; // ingredientes, sus cantidades y texturas
+	//unordered_map<_ecs::_ingredients_id, unordered_map<Texture*, int>::iterator> ingredients; // ingredientes y un iterador a su textura y cantidad
+	//unordered_map<Texture*, int> ingTextures; // textura y cantidad de un ingrediente
 	SDLUtils* sdl = SDLUtils::instance();
 	InputHandler* ih;
 	CartelManager* cartelM = CartelManager::instance();
 	int totalDifIngr;
+	const string FONT_PATH = "assets/Fonts/Arcadepix Plus.ttf";
+	const int OFFSET = 10;
+	Font* font;
+	Texture* texture;
 public:
 	constexpr static _ecs::_cmp_id id = _ecs::cmp_SM_INVENTORY;
 	BasketMarketComponent(GameObject* parent);
