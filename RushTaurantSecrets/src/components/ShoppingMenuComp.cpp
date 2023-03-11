b44fd34e3da47cc7ac029e309d4bba7ad739a46e
nanxiqin <nanxiqin@ucm.es>
2023-03-11 11:18:15 +0100

    mejorado menú de compras + input

--- RushTaurantSecrets/src/components/ShoppingMenuComp.cpp
+++ RushTaurantSecrets/src/components/ShoppingMenuComp.cpp
@@ -29,9 +29,13 @@ ShoppingMenuComp::~ShoppingMenuComp() {
 }
 void ShoppingMenuComp::handleEvents() {
 
-	//no sé q botón asignar
 	if (ih->joysticksInitialised()) {
-		//if (!ih->getButtonState(0, SDL_CONTROLLER_BUTTON_A)) ; 
+		if(ih->getButtonState(0, SDL_CONTROLLER_BUTTON_LEFTSHOULDER)) decreaseN();
+		else if (ih->getButtonState(0, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER)) decreaseN();
+		else if (ih->getButtonState(0, SDL_CONTROLLER_BUTTON_B && ing != NONE_ING)) {
+			basket->addToBasket(ing, number, totalPrice);
+			closeMenu();
+		}
 	}
 	else if (ih->isKeyDown(SDLK_LEFT)) decreaseN(); //aumentar cantidad
 	else if (ih->isKeyDown(SDLK_RIGHT)) increaseN(); //disminuir cantidad
@@ -46,10 +50,10 @@ void ShoppingMenuComp::render() {
 	Vector menuPos = { playerTransform->getPos().getX() + playerTransform->getW() / 2 - MENU_WIDTH / 2,
 		playerTransform->getPos().getY() + PLAYER_OFFSETY - MENU_HEIGHT };
 	menu->render(build_sdlrect(menuPos.getX(), menuPos.getY(), MENU_WIDTH, MENU_HEIGHT));
-
+	string aux = to_string(totalPrice);
 	numberTex->render(build_sdlrect(menuPos.getX() + NUM_OFFSETX, menuPos.getY() + NUM_OFFSETY, NUM_WIDTH, NUM_HEIGHT));
 	priceTex->render(build_sdlrect(menuPos.getX() + PRICE_OFFSETX, menuPos.getY() + PRICE_OFFSETY, PRICE_WIDTH, PRICE_HEIGHT));
-	totalPriceTex->render(build_sdlrect(menuPos.getX() + TOT_OFFSETX, menuPos.getY() + TOT_OFFSETY, TOT_WIDTH, TOT_HEIGHT));
+	totalPriceTex->render(build_sdlrect(menuPos.getX() + TOT_OFFSETX, menuPos.getY() + TOT_OFFSETY, TOT_WIDTH*aux.size(), TOT_HEIGHT));
 	ingTex->render(build_sdlrect(menuPos.getX() + ING_OFFSETX, menuPos.getY() + ING_OFFSETY, ING_WIDTH, ING_HEIGHT));
 }
 void  ShoppingMenuComp::increaseN() {
@@ -69,8 +73,8 @@ void  ShoppingMenuComp::openMenu(_ecs::_ingredients_id _id) {
 		number = 1;
 		price = _ecs::MarketIngs[ing - HARINA].price;
 		totalPrice = number * price;
-		numberTex = new Texture(sdl->renderer(), to_string(number), *font, build_sdlcolor(0xFAF2E6ff));
-		priceTex = new Texture(sdl->renderer(), to_string(price), *font, build_sdlcolor(0xFAF2E6ff));
+		numberTex = new Texture(sdl->renderer(), to_string(number), *font, build_sdlcolor(0xf3e5c2ff));
+		priceTex = new Texture(sdl->renderer(), to_string(price), *font, build_sdlcolor(0x504631ff));
 		ingTex = &((*sdl).images().at(to_string(ing)));
 		changePrice();
 	}
@@ -89,6 +93,6 @@ void  ShoppingMenuComp::closeMenu() {
 }
 void  ShoppingMenuComp::changePrice() {
 	totalPrice = number * price;
-	numberTex = new Texture(sdl->renderer(), to_string(number), *font, build_sdlcolor(0xFAF2E6ff));
-	totalPriceTex = new Texture(sdl->renderer(), to_string(totalPrice), *font, build_sdlcolor(0xFAF2E6ff));
+	numberTex = new Texture(sdl->renderer(), to_string(number), *font, build_sdlcolor(0xf3e5c2ff));
+	totalPriceTex = new Texture(sdl->renderer(), to_string(totalPrice), *font, build_sdlcolor(0x504631ff));
 }
