bbb74831e1d18e5230b36e6c86cb1ac3ad6b439e
Santienper <stienda@ucm.es>
2023-03-13 17:02:28 +0100

    Upsis 2

--- RushTaurantSecrets/src/scenes/UIRestaurant.h
+++ RushTaurantSecrets/src/scenes/UIRestaurant.h
@@ -24,7 +24,7 @@ private:
 	const int REP0 = 0, REP1 = 0, REP2 = 20, REP3 = 40, REP4 = 60, REP5 = 80;
 	const int FONTSIZE = 50, ICONSIZE = 48, ICONX = 20, ICONY = 15;
 	// parametrizable: 1000 (cada segundo) / 10000 (cada diez segundos)
-	const int TIME_CLOCK_REFRESH = 100, ANGLE = 90, ANGLE_UPDATE = 5;
+	const int TIME_CLOCK_REFRESH = 1000, ANGLE = 90, ANGLE_UPDATE = 5;
 
 	const int MAX_LAPS = 1;
 
