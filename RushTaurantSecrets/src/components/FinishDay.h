2f795a011e0a3320816a25ac63da7ae1ce6a9207
Santienper <stienda@ucm.es>
2023-03-16 18:07:53 +0100

    [WIP] FinishDay actualizado

--- RushTaurantSecrets/src/components/FinishDay.h
+++ RushTaurantSecrets/src/components/FinishDay.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "../structure/Component.h"
-#include "../scenes/UIRestaurant.h"
+#include "ClockComponent.h"
 
 class FinishDay : public Component {
 private:
-	UIRestaurant* ui;
+	ClockComponent* clock;
 public:
 	constexpr static _ecs::_cmp_id id = _ecs::cmp_FINISH_DAY;
 
 	FinishDay(GameObject* parent);
 	void update();
+	void initComponent();
 };
