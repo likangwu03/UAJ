c71007e09d060d170681277c65121bb84bc9b737
Jjianuo <Jwen@ucm.es>
2023-03-16 12:17:07 +0100

    Daily menu mejorado

--- RushTaurantSecrets/src/components/DailyMenuComp.h
+++ RushTaurantSecrets/src/components/DailyMenuComp.h
@@ -19,6 +19,16 @@ private:
 	vector<_ecs::DishInfo> randomMenu();
 	uint8_t menuSize;
 	uint8_t spriteSize;
+	struct rend
+	{
+		Texture* tex;
+		float x;
+		float y;
+	public:
+		rend(Texture* t, float _x, float _y) : tex(t), x(_x), y(_y) { };
+	};
+	vector<rend> textures;
+	vector<rend> prices;
 
 	bool murder;
 
@@ -26,7 +36,9 @@ private:
 public:
 	constexpr static _ecs::_cmp_id id = _ecs::cmp_DAILYMENU;
 	DailyMenuComp(GameObject* parent, float w, float h, _ecs::_cmp_id id, uint8_t mSize = 4);
+	virtual ~DailyMenuComp();
 
+	virtual void render();
 	vector<_ecs::DishInfo>* getMenu();
 };
 
