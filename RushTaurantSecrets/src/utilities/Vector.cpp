7c6bcb1b9091300c5324a1921e5c4fdf9ea0b654
Santienper <stienda@ucm.es>
2023-03-23 02:32:05 +0100

    ¡CUIDAO! Si este es el último commit, no hagáis pull

--- RushTaurantSecrets/src/utilities/Vector.cpp
+++
@@ -1,7 +0,0 @@
-#include "Vector.h"
-
-const Vector Vector::zero = Vector();
-const Vector Vector::right = Vector(1, 0);
-const Vector Vector::down = Vector(0, 1);
-const Vector Vector::left = Vector(-1, 0);
-const Vector Vector::up = Vector(0, -1);
