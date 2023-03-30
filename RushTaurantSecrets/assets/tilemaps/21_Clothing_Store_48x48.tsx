b24472a4deaf4176a21c449e1d2d693b24bfabe9
MattCastUCM <mattcast@ucm.es>
2023-03-30 03:34:17 +0200

    tilemap casita

---
+++ RushTaurantSecrets/assets/tilemaps/21_Clothing_Store_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="21_Clothing_Store_48x48" tilewidth="48" tileheight="48" tilecount="1072" columns="16">
+ <image source="../Sprites/Restaurant/Interiors/Theme_Sorter_48x48/21_Clothing_Store_48x48.png" trans="a79796" width="768" height="3216"/>
+</tileset>
