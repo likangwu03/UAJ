9fbb98b12838bf389ef77c99d78eee4582b621ec
MattCastUCM <mattcast@ucm.es>
2023-04-12 17:56:18 +0200

    Hora del día smooth, pero no del todo

--- RushTaurantSecrets/src/Components/TimeOfDay.cpp
+++ RushTaurantSecrets/src/Components/TimeOfDay.cpp
@@ -5,20 +5,41 @@
 #include "../Utilities/SDLUtils.h"
 #include "../Utilities/checkML.h"
 
-TimeOfDay::TimeOfDay(Texture* aftTexture, Texture* nightTexture, GameObject* parent) :Component(parent, _ecs::cmp_INVALID), topTexture(nullptr),
-afternoon(aftTexture), night(nightTexture) {}
+TimeOfDay::TimeOfDay(Texture* aftTexture, Texture* nightTexture, GameObject* parent) : 
+	Component(parent, _ecs::cmp_INVALID), topTexture(nullptr), afternoon(aftTexture), night(nightTexture), lastDeg(0), currOpac(0), isAfternoon(false), isNight(false) {
+
+	clock = ClockComponent::get();
+}
 
 void TimeOfDay::update() {
-	if (ClockComponent::get()->getRotation() >= AFTERNOON && ClockComponent::get()->getRotation() <= NIGHT)
-		topTexture = afternoon;
-	else if (ClockComponent::get()->getRotation() >= NIGHT)
-		topTexture = night;
-	else
+	if (clock->getRotation() < DAY && !isAfternoon && !isNight) {
 		topTexture = nullptr;
+		isAfternoon = false;
+		isNight = false;
+		currOpac = 0;
+	}
+	else if (clock->getRotation() >= DAY && clock->getRotation() && !isAfternoon) {
+ 		topTexture = afternoon;
+		isAfternoon = true;
+		currOpac = 0;
+	}
+	else if (clock->getRotation() >= AFTERNOON && !isNight) {
+		topTexture = night;
+		isNight = true;
+		currOpac = 0;
+	}
 }
 
 void TimeOfDay::render() {
 	if (topTexture != nullptr) {
+ 		topTexture->setOpacity(currOpac);
 		topTexture->render(build_sdlrect(0, 0, topTexture->width() * sdlutils().getResizeFactor(), topTexture->height() * sdlutils().getResizeFactor()));
+		
+		if (currOpac < 100) {
+			if (topTexture == afternoon)
+				currOpac += INC_OPAC_AFT;
+			else if (topTexture == night)
+				currOpac += INC_OPAC_NIGHT;
+		}
 	}
 }
--- RushTaurantSecrets/src/Components/TimeOfDay.h
+++ RushTaurantSecrets/src/Components/TimeOfDay.h
@@ -6,10 +6,16 @@ using namespace std;
 
 class TimeOfDay : public Component {
 private:
-	const int AFTERNOON = 130, NIGHT = 230, END = 360;
+	const int DAY = 15, AFTERNOON = 130, NIGHT = 230, END = 360;
+	const float INC_OPAC_AFT = AFTERNOON / 100.0f, INC_OPAC_NIGHT = (NIGHT-AFTERNOON) / 100.0f;
+	
+	ClockComponent* clock;
 	Texture* afternoon;
 	Texture* night;
-	Texture* topTexture;
+	Texture* topTexture; 
+	bool isAfternoon, isNight;
+	int lastDeg;
+	float currOpac;
 
 public:
 	TimeOfDay(Texture* aftTexture, Texture* nightTexture, GameObject* parent);
