ce072790d1a8d8dc5228f6febd8ccd7c660684b3
maria <marsache@ucm.es>
2023-04-24 17:35:39 +0200

    retratos json bad ending 4

---
+++ RushTaurantSecrets/src/Scenes/Cutscenes/BadEnding4Scene.cpp
@@ -0,0 +1,18 @@
+#include "BadEnding4Scene.h"
+
+BadEnding4Scene::BadEnding4Scene() {
+
+
+}
+
+void BadEnding4Scene::renderCinematic() {
+
+}
+
+void BadEnding4Scene::finishScene() {
+
+}
+
+void BadEnding4Scene::update() {
+
+}
---
+++ RushTaurantSecrets/src/Scenes/Cutscenes/BadEnding4Scene.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../../Scenes/Cutscenes/CinematicBaseScene.h"
+#include "../../GameObjects/Player.h"
+#include "../../Components/StraightMovement.h"
+#include <vector>
+
+using namespace std;
+struct dialogueInfo;
+
+class BadEnding4Scene: public CinematicBaseScene
+{
+private:
+	GameObject* player;
+	StraightMovement* straightMovement;
+	vector<dialogueInfo> dialogues;
+public:
+	BadEnding4Scene();
+	void renderCinematic() override;
+	void finishScene()override;
+	void update()override;
+};
