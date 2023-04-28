f21e6e38090420bf8ffc1cc882bdee8ac6d25693
likang03 <likangwu@ucm.es>
2023-04-28 16:17:46 +0200

    net

--- RushTaurantSecrets/src/Definitions/Message_def.h
+++ RushTaurantSecrets/src/Definitions/Message_def.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "../Utilities/Vector.h"
 #include "Food_def.h"
-
+#include <SDL.h>
+#include <iostream>
+#include <SDL_net.h>
+#include <cassert>
 // Mensajes para enviar a través de internet
 
 struct Message {
+	typedef char byte;
+	typedef uint8_t id_type;
+	struct doubleByte { byte first, second; };
+
 	enum _msg_id : uint8_t {
 		msg_PLAYER,
 		msg_BASKET,
@@ -15,7 +22,6 @@ struct Message {
 		msg_INVALID
 	};
 	_msg_id id;
-	
 
 	struct data_player {
 		Vector pos, vel;
@@ -33,7 +39,152 @@ struct Message {
 	} daily_menus;
 
 	struct data_DAILY_MENU {
-		uint8_t menu; 
+		uint8_t menu;
 	} daily_menu;
+
+
+
+#pragma region code_decode
+
+
+	/// <summary>
+   /// Para int: desde el -128 hasta el 127 /
+   /// Para uint: desde el 0 hasta el 255 /
+   /// Para char: todos los carácteres /
+   /// Para float: indefinido
+   /// </summary>
+	template<typename T>
+	Uint8* code8(T input, Uint8* msg) {
+		*reinterpret_cast<Uint8*>(msg) = input;
+		return msg + sizeof(Uint8);
+	}
+	template<typename T>
+	Uint8* decode8(Uint8& v, Uint8* msg) {
+		v =msg[0];
+		return msg + sizeof(Uint8);
+	}
+
+	Uint8* code8(int input, Uint8* msg) { return code8<int8_t>(input, msg); }
+
+	/// <summary>
+	/// Para int: desde el -32768 hasta el 32767 /
+	/// Para uint: desde el 0 hasta el 65535 /
+	/// Para float: desde el -3276.8 hasta el 3276.7
+	/// </summary>
+	template<typename T>
+	Uint8* code16(T input, Uint8* msg) {
+		doubleByte output{};
+		output.first = output.second = 0;
+		for (uint8_t i = 0; i < 8; i++, input >>= 1) {
+			output.second = (output.second << 1) + (input & 1);
+		}
+		for (uint8_t i = 0; i < 8; i++, input >>= 1) {
+			output.first = (output.first << 1) + (input & 1);
+		}
+		msg = code8(output.first, msg);
+		msg = code8(output.second, msg);
+		return msg;
+	}
+	template<typename T>
+	Uint8* decode16(T& v, Uint8* msg) {
+		Uint8 input[2];
+		msg = decode8<int8_t>(input[0], msg);
+		msg = decode8<int8_t>(input[1], msg);
+
+		v = *reinterpret_cast<Uint16*>(input);
+		/*for (uint8_t i = 0; i < 8; i++, input.first >>= 1) {
+			v = (v << 1) + (input.first & 1);
+		}
+		for (uint8_t i = 0; i < 8; i++, input.second >>= 1) {
+			v = (v << 1) + (input.second & 1);
+		}*/
+		return msg;
+	};
+	Uint8* code16(int input, Uint8* msg) { return code16<int16_t>(input, msg); };
+	Uint8* decode16(int& v, Uint8* msg) {
+		uint16_t aux;
+		msg = decode16(aux, msg);
+		v = aux;
+		return msg;
+	};
+	Uint8* code16(float input, Uint8* msg) { return code16<int16_t>(roundf(input * 10), msg); };
+	Uint8* decode16(float& v, Uint8* msg) {
+		uint16_t aux;
+		msg = decode16(aux, msg);
+		v = aux / 10.0f;
+		return msg;
+	};
+#pragma endregion
+public:
+	Uint8* code(Uint8* msg) {
+		msg = code8(id, msg);
+		switch (id) {
+		case Message::msg_PLAYER:
+			msg=code16(player.pos.getX(),msg);
+			msg=code16(player.pos.getY(),msg);
+			msg=code16(player.vel.getX(),msg);
+			msg=code16(player.vel.getY(),msg);
+			msg=code8(player.scene,msg);
+			break;
+		case Message::msg_BASKET:
+			msg=code8(basket.ing,msg);
+			msg=code8(basket.n,msg);
+			break;
+		case Message::msg_TO_DAILY_MENU:
+			for (uint8_t d : daily_menus.menu1) {
+				msg=code8(d,msg);
+			}
+			for (uint8_t d : daily_menus.menu2) {
+				msg=code8(d,msg);
+			}
+			break;
+		}
+		return msg;
+	}
+
+	// Recoge la información del buffer y ejecuta las acciones pertinentes
+	Uint8* decode(Uint8* msg) {
+		Message m{ };
+		m.id = id;
+		switch (id) {
+		case Message::msg_PLAYER:
+			float aux;
+			msg = decode16<float>(aux, msg);
+			player.pos.setX(aux);
+			msg = decode16<float>(aux, msg);
+			player.pos.setY(aux);
+			msg = decode16<float>(aux, msg);
+			player.vel.setX(aux);
+			msg = decode16<float>(aux, msg);
+			player.vel.setY(aux);
+			msg = decode8< uint8_t>(player.scene, msg);
+			break;
+		case Message::msg_BASKET:
+			uint8_t aux2;
+			msg = decode8<uint8_t>(aux2, msg);
+			basket.ing =(_ecs::_ingredients_id) aux2;
+			msg = decode8<uint8_t>(aux2, msg);
+			basket.n = aux2;
+			
+		case Message::msg_TO_DAILY_MENU:
+			for (int i = 0; i < 4; ++i) {
+				uint8_t aux;
+				msg = decode8<uint8_t>(aux, msg);
+				m.daily_menus.menu1.push_back((_ecs::_ingredients_id)aux);
+			}
+			for (int i = 0; i < 4; ++i) {
+				uint8_t aux;
+				msg = decode8<uint8_t>(aux, msg);
+				m.daily_menus.menu2.push_back((_ecs::_ingredients_id)aux);
+			}
+			break;
+		case Message::msg_TO_SUPERMARKET:
+
+			break;
+		}
+		return msg;
+	}
+
+
 
 };
--- RushTaurantSecrets/src/Utilities/CoopHandler.h
+++ RushTaurantSecrets/src/Utilities/CoopHandler.h
@@ -3,9 +3,7 @@
 #include <string>
 #include "../Definitions/Message_def.h"
 
-typedef char byte;
-typedef uint8_t id_type;
-struct doubleByte { byte first, second; };
+
 class GameManager;
 
 
@@ -19,13 +17,18 @@ private:
     static const int __CONNECTED_LENGTH = 10;
     // Mensaje recibido al conectarse al servidor (si todo ha ido bien)
     const char connected[__CONNECTED_LENGTH] = "Connected";
-
-    byte data[1024];
+    static constexpr std::size_t bufferSize_ = 1024;
+    Uint8 data[bufferSize_];
 
     int dataLength;
 
     void code(const Message& message);
+
     Message decode(Message::_msg_id id, uint16_t& last);
+
+    Uint8* receiveMsg(TCPsocket sock);
+    bool Receive(Message& v);
+    
 public:
     CoopHandler();
     ~CoopHandler();
@@ -48,65 +51,5 @@ public:
     void closeServer();
     void closeConnection();
     void receive();
-    void send(const Message& message);
-
-    // Locurote
-
-
-    /// <summary>
-    /// Para int: desde el -128 hasta el 127 /
-    /// Para uint: desde el 0 hasta el 255 /
-    /// Para char: todos los carácteres /
-    /// Para float: indefinido
-    /// </summary>
-    template<typename T>
-    void code8(T input) {
-        data[dataLength] = input;
-        dataLength++;
-    }
-    template<typename T>
-    T decode8(uint16_t& last) {
-        T output = (T)data[last];
-        last++;
-        return output;
-    }
-
-    template<> void code8<int>(int input) { code8<int8_t>(input); }
-
-    /// <summary>
-    /// Para int: desde el -32768 hasta el 32767 /
-    /// Para uint: desde el 0 hasta el 65535 /
-    /// Para float: desde el -3276.8 hasta el 3276.7
-    /// </summary>
-    template<typename T>
-    void code16(T input) {
-        doubleByte output;
-        output.first = output.second = 0;
-        for (uint8_t i = 0; i < 8; i++, input >>= 1) {
-            output.second = (output.second << 1) + (input & 1);
-        }
-        for (uint8_t i = 0; i < 8; i++, input >>= 1) {
-            output.first = (output.first << 1) + (input & 1);
-        }
-        data[dataLength] = output.first; data[dataLength + 1] = output.second;
-        dataLength += 2;
-    }
-    template<typename T>
-    T decode16(uint16_t& last) {
-        T output{}; std::pair<char, char> input;
-        input.first = data[last]; input.second = data[last + 1];
-        last += 2;
-        for (uint8_t i = 0; i < 8; i++, input.first >>= 1) {
-            output = (output << 1) + (input.first & 1);
-        }
-        for (uint8_t i = 0; i < 8; i++, input.second >>= 1) {
-            output = (output << 1) + (input.second & 1);
-        }
-        return output;
-    }
-
-    template<> void code16<int>(int input) { code16<int16_t>(input); }
-    template<> int decode16<int>(uint16_t& last) { return decode16<int16_t>(last); }
-    template<> void code16<float>(float input) { code16<int16_t>(roundf(input * 10)); }
-    template<> float decode16<float>(uint16_t& last) { return decode16<int16_t>(last) / 10.0f; }
+    bool send(Message& message);
 };
