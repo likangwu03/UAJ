a8442d6244c77d0974c6bbfdcbdcac18cb616875
Santienper <stienda@ucm.es>
2023-05-02 12:59:51 +0200

    Ahora se quita el dinero correspondiente

--- RushTaurantSecrets/src/Components/SuperCashRegisterTriggerComp.cpp
+++ RushTaurantSecrets/src/Components/SuperCashRegisterTriggerComp.cpp
@@ -82,9 +82,7 @@ void SuperCashRegisterTriggerComp::sendToClien() {
 }
 
 void SuperCashRegisterTriggerComp::receive(const Message& message) { 
-	/*if(message.id == Message::msg_TO_RESTAURANT) {
-		money->subtractMoney(money->getPrize());
-		money->setPrize(0);
+	if(message.id == Message::msg_TO_RESTAURANT) {
 		payAndLeave();
-	}*/
+	}
 }
